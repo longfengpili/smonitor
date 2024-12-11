9d346b9d0c61729958b8bde0d871d78214bdb0fd
longfengpili <398745129@qq.com>
2024-12-11 17:34:35 +0800

    [新增]startup增加 restart

--- process_exporter/startup.sh
+++ process_exporter/startup.sh
@@ -2,7 +2,7 @@
 # @Author: longfengpili
 # @Date:   2024-12-11 16:05:52
 # @Last Modified by:   longfengpili
-# @Last Modified time: 2024-12-11 17:31:26
+# @Last Modified time: 2024-12-11 17:34:13
 
 mkdir ~/procs
 cp ./startup.sh ~/procs/startup.sh
@@ -13,5 +13,6 @@ sudo cp procs_exporter.service /etc/systemd/system/procs_exporter.service
 sudo systemctl daemon-reload
 sudo systemctl enable procs_exporter.service
 sudo systemctl start procs_exporter.service
+sudo systemctl restart procs_exporter.service
 sudo systemctl status procs_exporter.service
 
